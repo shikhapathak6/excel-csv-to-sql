8c8fc8f7510336c841327b61e68f9ccb48e7d9e3
Shikha Pathak <shikha.pathak6@gmail.com>
2025-05-14 09:16:09 +0530

    Create combined_insert_ignore.sql

---
+++ files/combined_insert_ignore.sql
@@ -0,0 +1,6 @@
+INSERT IGNORE INTO `your_table_name` (`id`, `name`, `email`) VALUES ('1', 'Alice', ' alice@example.com');
+INSERT IGNORE INTO `your_table_name` (`id`, `name`, `email`) VALUES ('2', 'Bob', ' bob@example.com');
+INSERT IGNORE INTO `your_table_name` (`id`, `name`, `email`) VALUES ('3', 'Carol', ' carol@example.com');
+INSERT IGNORE INTO `your_table_name` (`id`, `name`, `email`) VALUES ('4', 'David', 'david@example.com');
+INSERT IGNORE INTO `your_table_name` (`id`, `name`, `email`) VALUES ('5', 'Eva', 'eva@example.com');
+INSERT IGNORE INTO `your_table_name` (`id`, `name`, `email`) VALUES ('6', 'Frank', 'frank@example.com');
